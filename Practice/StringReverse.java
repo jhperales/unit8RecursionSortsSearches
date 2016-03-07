3a76a082bfe6ab6bfdf9e37b65330b8ec8f7288f
jhperales <JHPerales@student.naperville203.org>
2016-03-07 14:14:25 -0600

    gfgf

---
+++ Practice/StringReverse.java
@@ -0,0 +1,26 @@
+public class StringReverse
+{
+    public static String reverseIter(String text)
+    {
+        String reverseText = "";
+        for (int i = text.length() - 1; i >= 0; i--)
+        {
+            char c = text.charAt(i);
+            reverseText += c;
+        }
+        return reverseText;
+    }
+    public static String reverse(String text)
+    {
+        if (text.length() == 0)
+        {
+            return text;
+        }
+        
+        char c = text.charAt(0);
+        String rest = text.substring(1);
+        String reverseText = reverse(rest);
+        reverseText += c;
+        return reverseText;
+    }
+}
