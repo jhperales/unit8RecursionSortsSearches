07290f8430005d81cd416dbe009409e3a7fe0672
jhperales <JHPerales@student.naperville203.org>
2016-03-11 14:04:49 -0600

    here is a thing

--- Practice/LogSpiralPanel.java
+++ Practice/LogSpiralPanel.java
@@ -7,25 +7,88 @@ import javax.swing.JPanel;
 
 public class LogSpiralPanel extends JPanel
 {
-   private static final double GOLDEN_MEAN = (1 + Math.sqrt(5)) / 2;
+    private static final double GOLDEN_MEAN =  (1 + Math.sqrt(5)) / 2;
+    private int angle;
 
-   public void paintComponent(Graphics g)
-   {
-     
-   }
-   
-   /**
-      Method that recursively draws a logarithmic spiral.
-      @param x The x-coordinate of the golden rectangle's upper-left corner  
-      @param y The y-coordinate of the golden rectangle's upper-left corner
-      @param side the smallest side size of the golden rectangle
-      @param angle the angle (0, 90, 180 or 270) where the top of the 
-      golden rectangle is located. For the outermost golden rectangle, 
-      the angle is 90.
-   */
-   private void recursiveDraw(Graphics2D g2, double x, double y, double side, int angle)
-   {
-      
-   }
-   
+    public void paintComponent(Graphics g)
+    {
+
+    }
+
+    /**
+    Method that recursively draws a logarithmic spiral.
+    @param x The upper left corner x-coordinate of the golden rectangle
+    @param y The upper left corner y-coordinate of the golden rectangle
+    @param side the smallest side size of the golden rectangle
+    @param angle The angle (0, 90, 180 or 270) where the top of the 
+    current golden rectangle is located. For the outermost golden 
+    rectangle, the angle is 90.
+     */
+    private void recursiveDraw(Graphics2D g2, double x, double y, double side, int ang)
+    {
+        if (side <= 0)
+        {
+            return;
+        }
+
+        Rectangle2D.Double rec = new Rectangle2D.Double(x , y, side, side);
+        this.angle = ang;
+
+        if (this.ange > 360)
+        {
+            this.angle += 90;
+        }
+        else
+        {
+            this.angle = 0;
+        }
+        
+        
+    }
+
+    /**
+    Draws the arc of the current iteration.
+    @param x The x-coordinate of the square's upper-left corner  
+    @param y The y-coordinate of the square's upper-left corner
+    @param side The size of the side of the square (or the arc's radius)
+    @param angle The angle (0, 90, 180 or 270) where the top of the 
+    current golden rectangle is located. For the outermost golden 
+    rectangle, the angle is 90.
+     */
+    private void drawArc(Graphics2D g2, double x, double y, double side, int angle)
+    {
+        double auxX = x;
+        double auxY = y;
+        if (angle == 0 || angle == 270 )
+        {
+            auxX = x - side;
+        }
+        if (angle == 270 || angle == 180)
+        {
+            auxY = y - side;
+        }
+        g2.draw( new Arc2D.Double(auxX, auxY, side * 2, side * 2, angle, 90, Arc2D.OPEN));
+    }   
+
+    private double calculateNewX(double x, double angle, double side, double newSide)
+    {
+        if (angle == 0)
+            x = x + side - newSide;
+        else if (angle == 90)
+            x = x + side;
+        else if (angle == 270)
+            x = x - newSide;
+        return x;
+    }
+
+    private double calculateNewY(double y, double angle, double side, double newSide)
+    {
+        if (angle == 0)
+            y = y + side;
+        else if (angle == 180)
+            y = y - newSide;
+        else if (angle == 270)
+            y = y + side - newSide;
+        return y;
+    }
 }
