6e73e75cfaed5c3606b035cebc6e00947aff82a1
jhperales <JHPerales@student.naperville203.org>
2016-03-17 14:12:23 -0500

    plez helo

--- FractalTreeLab/TreeComponent.java
+++ FractalTreeLab/TreeComponent.java
@@ -5,13 +5,50 @@ public class TreeComponent extends JPanel
 {
     private final int PANEL_WIDTH = 400;
     private final int PANEL_HEIGHT = 600;
-    private double starting_angle;
-    private int current_order;
+    private double startingAngle;
+    private double fractionLength;
+    private double smallestBranch;
+    private int currentOrder;
     
     public TreeComponent()
     {
-        this.current_order = 1;
+        this.currentOrder = 1;
+        this.fractionLength = 0.3;
         this.setBackground(Color.BLACK);
         this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+        this.smallestBranch = 0.9;
+        this.startingAngle = 45;
     }
+    
+    public static void drawFractal(double length, int startX, int startY, double angle, Graphics2D g2)
+    {
+        double angle1, angle2, deltaX, deltaY;
+        
+        angle1 = angle + startingAngle;
+        angle2 = angle - startingAngle;
+        
+        if (order == 1)
+        {
+            g2.draw(startX, startY, endX, endY);
+        }
+        else
+        {
+            int endX1, endY1, endX2, endY2;
+            
+            length = length * this.fractionLength;
+            endX1 = (int)(startX - length*Math.sin(angle1));
+            endY1 = (int)(startY - length*Math.cos(angle1));
+            
+            g2.draw(new Line2D.Double(startX, staryY, endX1, endY2));
+            
+            drawFractal(length, startX, startY, angle, g2);
+        }
+            
+    }
+    
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+    }
+   
 }
