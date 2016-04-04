bf866ec3ed731c66bed0a22c35d30d1bee2a6e00
jhperales <JHPerales@student.naperville203.org>
2016-04-04 14:13:57 -0500

    began sorting

---
+++ MergeSort/SelectionSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class SelectionSort
+{
+    public static void sort(int[] a)
+    {
+        for (int i = 0; i < a.length; i++)
+        {
+            int minPos = minimumPosition(a, i);
+            ArrayUtil.swap(a ,minPos, i);
+        }
+    }
+    
+    private static int minimumPosition(int[] a, int from)
+    {
+        int minPos = from;
+        for (int i = from + 1; i < a.length; i++)
+        {
+            if (a[i] < a[minPos])
+            {
+                minPos = i;
+            }
+        }
+        return minPos;
+    }
+    
+    public static void main(String[] args)
+    {
+        int[] a = ArrayUtil.randomIntArray(20, 100);
+        System.out.println("Unsorted: " + Arrays.toString(a));
+        
+        SelectionSort.sort(a);
+        System.out.println("Sorted: " + Arrays.toString(a));
+    }
+}
